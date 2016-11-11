b478af8eaa845ed473c8aa6ae55d1a11023e0872
Andrew <ajestkov@gmail.com>
2016-11-12 01:19:04 +0500

    gogo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 
+app.use(cors());
+
 app.get('/', function (req, res) {
-  res.send('test');
+  var fullname = req.query.fullname ? req.query.fullname : undefined;
+  var names = [];
+  if (fullname != undefined) {
+    var re = new RegExp('[0-9]+');
+    if (re.test(fullname)) {
+      res.status(200).send('Invalid fullname');
+      return;
+    }
+    names = fullname.split(' ');
+
+
+
+    switch (names.length) {
+      case 1:
+        res.status(200).send(fullname);
+        break;
+      case 2:
+        res.status(200).send(names[1] + ' ' + names[0].charAt(0) + '.');
+        break;
+      case 3:
+        res.status(200).send(names[2] + ' ' + names[0].charAt(0) + '. ' +
+      names[1].charAt(0) + '.');
+        break;
+      default:
+        res.status(200).send('Invalid fullname');
+        break;
+    }
+  }
+  else {
+    res.status(200).send('Invalid fullname');
+  }
+
 });
 
 app.listen(3000, function () {
-  console.log('Example app listeting port 3000');
+  console.log('Example app listening on port 3000!');
 });
